ed7bcfd23710e46b08532cb3c7475fd966242d9d
Egor <zuzik17@yandex.ru>
2025-01-05 19:51:20 +0300

    Finish refacroting files in project

--- Simple3DViewer/src/main/java/com/cgvsu/Egor/triangle_rasterisation/color/Texture.java
+++
@@ -1,9 +0,0 @@
-package com.cgvsu.Egor.triangle_rasterisation.color;
-
-
-import com.cgvsu.Egor.triangle_rasterisation.math.Barycentric;
-
-public interface Texture {
-    public ColorRGB get(final Barycentric b);
-}
-
